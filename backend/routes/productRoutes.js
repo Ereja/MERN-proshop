431c449746f422adbf5543140a5d7bcfe2829981
Vaida <93vaida@gmail.com>
2021-04-21 11:58:14 +0200

    backend routes, data fetch from database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+const router = express.Router();
+
+//description: fetches all prdocuts
+//route: GET /api/products
+//access: Public, anyone can access
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+//description: fetches specific product
+//route: GET /api/product/:id
+//access: Public, anyone can access
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product Not Found" });
+    }
+  })
+);
+
+export default router;
