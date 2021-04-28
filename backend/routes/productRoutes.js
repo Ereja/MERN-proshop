60045cb9c5e649e4da1770ca6a2a7ca136c5bc2a
Vaida <93vaida@gmail.com>
2021-04-28 10:48:18 +0200

    created a controllers, to have more clear router functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,12 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-//description: fetches all prdocuts
-//route: GET /api/products
-//access: Public, anyone can access
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-//description: fetches specific product
-//route: GET /api/product/:id
-//access: Public, anyone can access
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product Not Found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
